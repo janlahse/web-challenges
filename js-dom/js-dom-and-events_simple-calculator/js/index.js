343dc2f7f24a739debd7e85389480e0e96b881f6
Jan <jan.lahse@gmx.de>
2025-04-02 16:11:58 +0200

    assigned each button to a js variable

--- js-dom/js-dom-and-events_simple-calculator/js/index.js
+++ js-dom/js-dom-and-events_simple-calculator/js/index.js
@@ -8,7 +8,11 @@ const operand2 = 4;
 // Step 1: Use `document.querySelector` to select each button by its `data-js` attribute.
 
 // --v-- write your code here --v--
-
+const addButton = document.querySelector("[data-js=add]");
+const subButton = document.querySelector("[data-js=subtract]");
+const mulButton = document.querySelector("[data-js=multiply]");
+const divButton = document.querySelector("[data-js=divide]");
+const expButton = document.querySelector("[data-js=exponent]");
 // --^-- write your code here --^--
 
 /* 
