cb8d4a5116cc3ee7db27d95a8e4222b99cfa9505
Jan <jan.lahse@gmx.de>
2025-04-03 14:19:50 +0200

    introduced switch statement for the weather status

--- js-conditions/js-conditions-and-booleans_weather-check/js/index.js
+++ js-conditions/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,5 +1,17 @@
 // Change this value to test different "weather" conditions.
-const weather = "rainy"
+const weather = "sunny";
 
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22; 
+const temperature = 22;
+
+switch (weather) {
+  case "sunny":
+    console.log("It's sunny!");
+    break;
+  case "rainy":
+    console.log("It's rainy, don't forget your umbrella!");
+    break;
+  case "snowy":
+    console.log("It's snowy, stay warm!");
+    break;
+}
