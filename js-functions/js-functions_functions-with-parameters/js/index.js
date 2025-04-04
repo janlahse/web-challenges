df792bf7eda172a4fcedbcd984de9492237e8404
Jan <jan.lahse@gmx.de>
2025-04-04 16:12:04 +0200

    javascript functions with parameters

--- js-functions/js-functions_functions-with-parameters/js/index.js
+++ js-functions/js-functions_functions-with-parameters/js/index.js
@@ -1,49 +1,35 @@
 console.clear();
 
-console.log("-------------");
-console.log("Exercise 1:");
+seperator(1);
 /*
 1: The function below is not executed. Can you figure out why? Find the bug and fix it.
 */
 
 // Uncomment the next line of code and figure out why it does not work:
-// logText;
+logText();
 
 function logText() {
   console.log("This text does not appear. Why?");
 }
 
-console.log("-------------");
-console.log("Exercise 2:");
+seperator(2);
 /*
 2: Look at the functions below. They are almost identical. Can you find a way to generalize them into a new function with an input parameter? 
    Replace the function calls below with your new function.
-*/
+   
+*/ // --v-- write your code here --v--
 
-function greetBob() {
-  console.log("Welcome Bob, good to see you again!");
+function greet(name) {
+  console.log("Welcome " + name + ", good to see you again!");
 }
 
-function greetAlice() {
-  console.log("Welcome Alice, good to see you again!");
-}
-
-function greetMary() {
-  console.log("Welcome Mary, good to see you again!");
-}
-
-greetAlice();
-greetBob();
-greetMary();
-
-// --v-- write your code here --v--
-
-function greet() {}
+greet("Alice");
+greet("Bob");
+greet("Mary");
 
 // --^-- write your code here --^--
 
-console.log("-------------");
-console.log("Exercise 3:");
+seperator(3);
 /*
 3: We log a small separation text for each subtask. This is repetitive code, so let’s write a function `logSeparator` that takes the number of the exercise as an input parameter and logs the corresponding separator.
    Then, use this function to replace the existing separator logs in this JavaScript file.
@@ -52,5 +38,10 @@ Note: Separation text refers to the text that is logged in the console before ea
 */
 
 // --v-- write your code here --v--
+
+function seperator(number) {
+  console.log("-------------");
+  console.log("Exercise " + number + ":");
+}
 
 // --^-- write your code here --^--
