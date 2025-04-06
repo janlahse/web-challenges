5a8b5e55e0f650eca49aeec3354efd9a759ccd94
Jan <jan.lahse@gmx.de>
2025-04-06 17:59:45 +0200

    wrote calculateScrollPercentage function

--- js-functions/js-functions_progress-bar/js/index.js
+++ js-functions/js-functions_progress-bar/js/index.js
@@ -2,4 +2,14 @@ console.clear();
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-function calculateScrollPercentage() {}
+// - This function should calculate and return the percentage of the page that the user has scrolled.
+// - Use the following properties:
+//   - `window.scrollY`: The Y position of the window on the total webpage.
+//   - `window.innerHeight`: The height of the visible part of the window.
+//   - `document.body.clientHeight`: The total height of the webpage.
+
+function calculateScrollPercentage() {
+  const progress =
+    window.scrollY / (document.body.clientHeight - window.innerHeight);
+  return progress;
+}
