064c52705ec66676e2c89851ec5859a7455cb435
Jan <jan.lahse@gmx.de>
2025-04-06 18:28:52 +0200

    added eventlistener to update scrollbar

--- js-functions/js-functions_progress-bar/js/index.js
+++ js-functions/js-functions_progress-bar/js/index.js
@@ -2,14 +2,9 @@ console.clear();
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-// - This function should calculate and return the percentage of the page that the user has scrolled.
-// - Use the following properties:
-//   - `window.scrollY`: The Y position of the window on the total webpage.
-//   - `window.innerHeight`: The height of the visible part of the window.
-//   - `document.body.clientHeight`: The total height of the webpage.
+document.addEventListener("scroll", () => {
+  progressBar.style.width = calculateScrollPercentage() * 100 + "%";
+});
 
-function calculateScrollPercentage() {
-  const progress =
-    window.scrollY / (document.body.clientHeight - window.innerHeight);
-  return progress;
-}
+const calculateScrollPercentage = () =>
+  window.scrollY / (document.body.clientHeight - window.innerHeight);
