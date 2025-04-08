0b415ed332dae8e242946bc3a2bc70a59b524d0d
Jan <jan.lahse@gmx.de>
2025-04-08 15:54:54 +0200

    added functionality to buttons to create and clear toast messages

--- js-createelement/js-createelement_toast-messages/js/index.js
+++ js-createelement/js-createelement_toast-messages/js/index.js
@@ -6,8 +6,13 @@ const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
   // Exercise: Append a new entry to the toast messages container
+  const toast = document.createElement("li");
+  toast.classList.add("toast-container__message");
+  toast.textContent = "I'm a toast message.";
+  toastContainer.append(toast);
 });
 
 clearButton.addEventListener("click", () => {
   // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
