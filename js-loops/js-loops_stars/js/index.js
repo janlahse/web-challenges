8d9c0163132ee0baf8b83dbd9d8a0f80d8ee0963
Jan <jan.lahse@gmx.de>
2025-04-10 15:12:27 +0200

    added eventlistener to star to fill every star before and including the clicked star

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,14 +2,21 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
-  // Reset the star container before re-rendering stars
+function renderStars(filledStars) {
   starContainer.innerHTML = "";
-
   // --v-- write or modify code below this line --v--
-  for (i = 1; i <= 5; i++) {
+  for (let i = 1; i <= 5; i++) {
     const star = document.createElement("img");
-    star.src = "assets/star-empty.svg";
+    if (i <= filledStars) {
+      star.src = "assets/star-filled.svg";
+    } else {
+      star.src = "assets/star-empty.svg";
+    }
+
+    star.addEventListener("click", () => {
+      renderStars(i);
+    });
+
     starContainer.append(star);
   }
   // --^-- write or modify code above this line --^--
