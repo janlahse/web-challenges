a0af8d3ad8b2568ed5098fb498b19e2fd0452292
Jan <jan.lahse@gmx.de>
2025-04-10 12:23:54 +0200

    enabled star display

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -7,7 +7,11 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-
+  for (i = 1; i <= 5; i++) {
+    const star = document.createElement("img");
+    star.src = "assets/star-empty.svg";
+    starContainer.append(star);
+  }
   // --^-- write or modify code above this line --^--
 }
 
