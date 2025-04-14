46ef84bd392505d9287844ada2221ecdc25ae6fb
Jan <jan.lahse@gmx.de>
2025-04-14 11:28:38 +0200

    fetched data from api and output it in the console

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,12 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const data = await fetch(url);
+  const output = await data.json();
+  console.log(output);
+  console.log(output.results[0]);
+  console.log(output.results[2].eye_color);
+}
 
 fetchData();
