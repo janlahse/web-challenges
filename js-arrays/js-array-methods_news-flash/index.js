7105d6c23bc4cb18c7f75f1b243308bae479a51b
Jan <jan.lahse@gmx.de>
2025-04-20 21:04:42 +0200

    filtered and sorted news site

--- js-arrays/js-array-methods_news-flash/index.js
+++ js-arrays/js-array-methods_news-flash/index.js
@@ -5,12 +5,14 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
+const filteredNews = news.filter((card) => {
+  return card.categories.includes("politics");
 });
 
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.toSorted(
+  (short, long) => short.body.length - long.body.length
+);
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
