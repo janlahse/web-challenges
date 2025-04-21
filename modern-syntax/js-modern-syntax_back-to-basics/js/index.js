135ec364f86fa97bbeb52dba285143afd7d371a7
Jan <jan.lahse@gmx.de>
2025-04-21 12:10:39 +0200

    translated modern js syntax into older syntax

--- modern-syntax/js-modern-syntax_back-to-basics/js/index.js
+++ modern-syntax/js-modern-syntax_back-to-basics/js/index.js
@@ -3,15 +3,28 @@ Now that you've practiced destructuring, default parameters, and the spread oper
 try reversing the challenge by rewriting this code without using these modern features.
 */
 
-export const getNameAndCountry = ({ name, country }) => [name, country];
+//export const getNameAndCountry = ({ name, country }) => [name, country];
+export function getNameAndCountry({ name, country }) {
+  return [name, country];
+}
 
-export const getRelocatedCity = (
+export function getRelocatedCity( //gets two city objects
   city1,
-  city2 = { name: "Berlin", country: "Germany" }
-) => {
-  const [, country] = getNameAndCountry(city2);
+  //city2 = { name: "Berlin", country: "Germany" }
+  //sets default for varaible
+  city2
+) {
+  if (city2 == undefined) {
+    city2 = { name: "Berlin", country: "Germany" };
+  }
+  //const [, country] = getNameAndCountry(city2);
+  //turns object into array, declares country varaible as city2.country
+  const country = city2.country;
   return {
-    ...city1,
+    //  ...city1,
+    //spreads the city1 object
+    name: city1.name,
+    country: city1.country,
     country,
   };
-};
+}
