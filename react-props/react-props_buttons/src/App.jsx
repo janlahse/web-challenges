3ef2f65be91df5a9da703fa234e0406b93a4e935
Jan <jan.lahse@gmx.de>
2025-04-22 16:44:49 +0200

    wrote a button component with click event handler and passed the functions as a prop

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,3 +1,26 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("Oh yes, you clicked the Button");
+  }
+
+  return (
+    <Button
+      color="#abc"
+      disabled={false}
+      text="Buttoning. Yeah."
+      onClick={handleClick}
+    />
+  );
+}
+
+function Button({ color, disabled, text, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      style={{ height: "80px", backgroundColor: color }}
+    >
+      {text}
+    </button>
+  );
 }
