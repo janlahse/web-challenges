e9bde66cbb414405b1444cc95d68f0df4f32e12c
Jan <jan.lahse@gmx.de>
2025-04-25 15:21:19 +0200

    enabled add and remove tag

--- react-state-3/react-state-3_tags/src/App.jsx
+++ react-state-3/react-state-3_tags/src/App.jsx
@@ -6,10 +6,22 @@ import List from "./components/List";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    setTags([...tags, newTag]);
+  }
+
+  function handleRemoveTag(oldTag) {
+    setTags(
+      tags.filter((tag) => {
+        return tag !== oldTag;
+      })
+    );
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleRemoveTag} />
     </main>
   );
 }
