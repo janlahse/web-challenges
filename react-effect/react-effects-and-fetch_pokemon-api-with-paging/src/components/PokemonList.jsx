f4d3fa28e307e00d67b809a34dd68b1be7b8d71d
Jan <jan.lahse@gmx.de>
2025-04-28 12:09:43 +0200

    enabled switch page buttons

--- react-effect/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
+++ react-effect/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [pageOffset, setPageOffset] = useState(0);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${pageOffset}`
         );
         const data = await response.json();
         setPokemon(data.results);
@@ -17,12 +18,20 @@ export default function PokemonList() {
     }
 
     loadPokemon();
-  }, []);
+  }, [pageOffset]);
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button
+        type="button"
+        onClick={() => setPageOffset(pageOffset - 20)}
+        disabled={pageOffset == 0 && true}
+      >
+        Previous Page
+      </button>
+      <button type="button" onClick={() => setPageOffset(pageOffset + 20)}>
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
