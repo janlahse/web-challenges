dcccd07f2dd61eac21519a5ec2d427e53c837e5a
Jan <jan.lahse@gmx.de>
2025-05-05 16:14:04 +0200

    enabled dynamic routes and added random volume button

--- next-basics/nextjs-dynamic-routes_lotr-app-dynamic-routes/components/RandomButton.js
+++ next-basics/nextjs-dynamic-routes_lotr-app-dynamic-routes/components/RandomButton.js
@@ -1,3 +1,11 @@
-export function RandomButton({ onClick }) {
-  return <button onClick={onClick}>Random Volume</button>;
+import { useRouter } from "next/router";
+import { volumes } from "@/lib/data";
+
+export function RandomButton({}) {
+  const router = useRouter();
+  function handleRandom() {
+    const randomIndex = Math.floor(Math.random() * volumes.length);
+    router.push(`/volumes/${volumes[randomIndex].slug}`);
+  }
+  return <button onClick={handleRandom}>Random Volume</button>;
 }
--- next-basics/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
+++ next-basics/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -1,20 +1,13 @@
 import Link from "next/link";
 import { introduction, volumes } from "../../lib/data";
 import { RandomButton } from "@/components/RandomButton";
-import { useRouter } from "next/router";
 
 export default function Volumes() {
-  const router = useRouter();
-  function handleRandom() {
-    const randomIndex = Math.floor(Math.random() * volumes.length);
-    router.push(`/volumes/${volumes[randomIndex].slug}`);
-  }
-
   return (
     <>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
-      <RandomButton onClick={handleRandom}></RandomButton>
+      <RandomButton />
       <ul>
         {volumes.map(({ slug, title }) => (
           <li key={slug}>
