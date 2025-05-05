206615ab9b71fdd9f238f0a41f398148a8495493
Jan <jan.lahse@gmx.de>
2025-05-05 13:01:34 +0200

    added pages to the lotr app, including dynamic content

---
+++ next-basics/lotr-app/pages/volumes/the-return-of-the-king.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { volumes } from "@/lib/data";
+import Image from "next/image";
+
+export default function Volume3() {
+  const volume = volumes.find(
+    (volume) => volume.slug === "the-return-of-the-king"
+  );
+  return (
+    <div>
+      <h1>{volume.title}</h1>
+      <Link href="/volumes">← All Volumes</Link>
+      <p>{volume.description}</p>
+      <ul>
+        <li>
+          {volume.books[0].ordinal}: {volume.books[0].title}
+        </li>
+        <li>
+          {volume.books[1].ordinal}: {volume.books[1].title}
+        </li>
+      </ul>
+      <Image alt="cover" src={volume.cover} width={140} height={230}></Image>
+    </div>
+  );
+}
