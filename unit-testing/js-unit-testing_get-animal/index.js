f59ffcaa8728f31579c28fb7c76a08a89c905c2c
Jan <jan.lahse@gmx.de>
2025-05-06 16:57:11 +0200

    wrote function that meets the test requirements

--- unit-testing/js-unit-testing_get-animal/index.js
+++ unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,7 @@
-export function getAnimal() {}
+export function getAnimal(animals) {
+  return animals === undefined
+    ? "I do not like animals at all!"
+    : animals === "cats"
+    ? `I totally love cats!`
+    : `I like ${animals}!`;
+}
