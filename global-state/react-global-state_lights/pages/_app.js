1d81e70c7c924d54f4071db1afa9b74c018c9e00
Jan <jan.lahse@gmx.de>
2025-05-08 20:24:05 +0200

    lifted up the light state to make it global, added functionality to turn all light on/off buttons

--- global-state/react-global-state_lights/pages/_app.js
+++ global-state/react-global-state_lights/pages/_app.js
@@ -1,11 +1,48 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const initialLights = [
+    { id: 1, name: "Living Room", isOn: false },
+    { id: 2, name: "Kitchen", isOn: false },
+    { id: 3, name: "Bedroom", isOn: false },
+    { id: 4, name: "Bathroom", isOn: false },
+    { id: 5, name: "Garage", isOn: false },
+    { id: 6, name: "Porch", isOn: false },
+    { id: 7, name: "Garden", isOn: false },
+    { id: 8, name: "Office", isOn: false },
+  ];
+  const [lights, setLights] = useState(initialLights);
+  const lightCount = lights.reduce((a, b) => a + (b.isOn ? 1 : 0), 0);
+
+  function handleToggleLight(id) {
+    setLights(
+      lights.map((light) => {
+        return light.id === id ? { ...light, isOn: !light.isOn } : light;
+      })
+    );
+  }
+
+  function handleTurnLightsOn() {
+    setLights(lights.map((light) => ({ ...light, isOn: true })));
+  }
+
+  function handleTurnLightsOff() {
+    setLights(lights.map((light) => ({ ...light, isOn: false })));
+  }
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        lights={lights}
+        onToggleLight={handleToggleLight}
+        lightCount={lightCount}
+        onTurnLightsOn={handleTurnLightsOn}
+        onTurnLightsOff={handleTurnLightsOff}
+      />
     </Layout>
   );
 }
