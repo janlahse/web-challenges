cdea3cedd24f161bd83926a2db363e788e519058
Jan <jan.lahse@gmx.de>
2025-05-08 12:23:24 +0200

    refactored the fetching to use swr

--- react-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
+++ react-fetching/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -1,10 +1,28 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
+//import { useSWR, isLoading, error } from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
 export default function ISSTracker() {
+  const fetcher = (...args) =>
+    fetch(...args).then((response) => response.json());
+
+  const {
+    data: coords,
+    error,
+    isLoading,
+    mutate,
+  } = useSWR(URL, fetcher, { refreshInterval: 5000 });
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>{error.message}</h1>;
+  }
+  /*
   const [coords, setCoords] = useState({
     longitude: 0,
     latitude: 0,
@@ -31,14 +49,14 @@ export default function ISSTracker() {
       clearInterval(timer);
     };
   }, []);
-
+  */
   return (
     <main>
       <Map longitude={coords.longitude} latitude={coords.latitude} />
       <Controls
         longitude={coords.longitude}
         latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        onRefresh={() => mutate()}
       />
     </main>
   );
