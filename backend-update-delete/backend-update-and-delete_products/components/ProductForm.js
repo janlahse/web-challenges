03a347d3568b81f5ef3a2320142c88c27bff7851
Jan <jan.lahse@gmx.de>
2025-05-15 12:49:33 +0200

    added PUT and DELETE requests and buttons

--- backend-update-delete/backend-update-and-delete_products/components/ProductForm.js
+++ backend-update-delete/backend-update-and-delete_products/components/ProductForm.js
@@ -1,36 +1,12 @@
 import styled from "styled-components";
-import useSWR from "swr";
 import StyledButton from "@/components/Button";
 
-export default function ProductForm() {
-  const { mutate } = useSWR("/api/products");
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const productData = Object.fromEntries(formData);
-
-    const response = await fetch("/api/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productData),
-    });
-
-    if (!response.ok) {
-      console.error(response.status);
-      return;
-    }
-
-    mutate();
-    event.target.reset();
-  }
-
+export default function ProductForm({ onSubmit, isEditMode }) {
   return (
-    <StyledForm onSubmit={handleSubmit}>
-      <StyledHeading>Add a new Fish</StyledHeading>
+    <StyledForm onSubmit={onSubmit}>
+      <StyledHeading>
+        {isEditMode ? "Edit Fish" : "Add a new Fish"}
+      </StyledHeading>
       <StyledLabel htmlFor="name">
         Name:
         <input type="text" id="name" name="name" />
--- backend-update-delete/backend-update-and-delete_products/pages/[id].js
+++ backend-update-delete/backend-update-and-delete_products/pages/[id].js
@@ -2,12 +2,40 @@ import useSWR from "swr";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import StyledLink from "@/components/Link";
+import { useState } from "react";
+import ProductForm from "@/components/ProductForm";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
+  const [isEditMode, setIsEditMode] = useState(false);
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) mutate();
+  }
+
+  async function handleDeleteProduct() {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) router.push("/");
+    else console.log("error: " + response.status);
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -25,6 +53,18 @@ export default function Product() {
         Price: {data.price} {data.currency}
       </p>
       <StyledLink href="/">Back to all</StyledLink>
+      <Button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Edit
+      </Button>
+      <Button type="button" onClick={() => handleDeleteProduct(id)}>
+        Delete
+      </Button>
+      {isEditMode && <ProductForm onSubmit={handleEditProduct} isEditMode />}
     </ProductCard>
   );
 }
@@ -32,4 +72,20 @@ export default function Product() {
 const ProductCard = styled.article`
   padding: 0.5rem 1rem;
   box-shadow: 0px 1px 5px -2px var(--color-granite);
+`;
+
+const Button = styled.button`
+  text-decoration: none;
+  display: inline-block;
+  color: inherit;
+  border: none;
+  background: var(--color-water-10);
+  font-size: larger;
+  padding: 0.5rem 1rem;
+  box-shadow: 0px 1px 5px -2px var(--color-granite);
+  margin-left: 5px;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
